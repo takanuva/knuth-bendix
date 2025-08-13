ad50049f9428f2406513bc868b0af73ce4998700
Paulo Torrens <paulotorrens@gnu.org>
2025-08-13 18:27:46 +0100

    Add equations and formulas

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,28 @@
-import { Term, Substitution, unify } from "./substitution.js"
+import { Term } from "./substitution.js"
 import * as kb from "./knuth-bendix.js"
 
-function v(name: number): Term {
-    return { type: "var", name }
-}
-
-function f(name: string, ...args: Term[]): Term {
-    return { type: "fun", name, args };
-}
-
-function show(subst: Substitution | false): string {
-    function show_term(term: Term): string {
-        if(term.type === "var") {
-            return term.name.toString();
-        }
-
-        return `${term.name}(${term.args.map(show_term).join(", ")})`;
-    }
-
-    function show_entry([key, value]: [number, Term]): string {
-        return `${key} => ${show_term(value)}`;
-    }
-
-    if(subst) {
-        const entries = subst.entries();
-        return entries.map(show_entry).toArray().join(", ");
-    }
-
-    return "false";
-}
-
-console.log(show(unify(f("foo"), v(0))));
-console.log(show(unify(f("foo"), f("bar"))));
-console.log(show(unify(f("likes", f("bob"), v(1)), f("likes", v(4), f("apple")))));
-
-console.log(show(unify(
-    f("append", f("nil"), v(0), v(0)),
-    f("append", f("nil"), f("cons", f("zero"), f("nil")), v(1))
-)));
-
 const test = new kb.KnuthBendix();
+test.addEquation(// app
+    ["inst", "s", ["app", "a", "b"]],
+    ["app", ["inst", "s", "a"], ["inst", "s", "b"]]
+)
+test.addEquation(// varid
+    ["inst", ["id"], ["z"]],
+    ["z"]
+)
+test.addEquation(// varcons
+    ["inst", ["cons", "a", "s"], ["z"]],
+    "a"
+)
+test.addEquation(// clos
+    ["inst", "t", ["inst", "s", "a"]],
+    ["inst", ["comp", "s", "t"], "a"]
+)
+test.addEquation(// abs
+    ["inst", "s", ["lam", "a"]],
+    ["lam", ["inst", ["cons", ["z"], ["comp", "s", ["shift"]]], "a"]]
+)
+test.addEquation(//idl
+    ["comp", ["id"], "s"],
+    "s"
+)
--- src/knuth-bendix.ts
+++ src/knuth-bendix.ts
@@ -10,13 +10,67 @@ export interface Equation {
     rhs: Term;
 }
 
+export type Formula = string | [string, ...Formula[]]
+
 export class KnuthBendix {
     private equations: Equation[];
     private rules: Rule[];
+    private names: Map<number, string>;
 
     public constructor() {
         this.equations = [];
         this.rules = [];
+        this.names = new Map();
+    }
+
+    private getVar(name: string, variables: Map<string, number>): number {
+        if(variables.has(name)) {
+            return variables.get(name)!;
+        }
+
+        let index = this.names.size;
+        variables.set(name, index);
+        this.names.set(index, name);
+        return index;
+    }
+
+    private parse(f: Formula, variables: Map<string, number>): Term {
+        if(typeof f === "string") {
+            let name = this.getVar(f, variables);
+            return { type: "var", name };
+        }
+
+        let [name, ...data] = f;
+        let args = data.map((x) => this.parse(x, variables));
+        return { type: "fun", name, args }
+    }
+
+    public addEquation(fst: Formula, snd: Formula) {
+        let variables = new Map();
+
+        let lhs = this.parse(fst, variables);
+        let rhs = this.parse(snd, variables);
+
+        let size = this.equations.length;
+        let lhs_str = this.showTerm(lhs);
+        let rhs_str = this.showTerm(rhs);
+        console.log(`Adding equation #${size}: ${lhs_str} = ${rhs_str}.`);
+
+        this.equations.push({ lhs, rhs });
+    }
+
+    private showTerm(term: Term): string {
+        if(term.type === "var") {
+            return this.names.get(term.name)!.toUpperCase();
+        }
+
+        let name = term.name;
+        let args = term.args.map((x) => " " + this.showTerm(x)).join("");
+        return `(${name}${args})`;
+    }
+
+    private addRule(fst: Term, snd: Term) {
+
     }
 
     public *listDelete() {
